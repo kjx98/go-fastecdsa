69b284851ba15f2fc6e3cf626b1dfa328fc69ceb
Jesse Kuang <jkuang@21cn.com>
2020-12-10 00:40:17 +0800

    add ecc c files

---
+++ ecc/ecc.go
@@ -0,0 +1,63 @@
+// +build amd64 arm64
+
+package ecc
+
+// #cgo CFLAGS: -O2
+// #include <sys/types.h>
+// #include "ecc.h"
+/*
+static struct ecc_point *getECC_point(u_int64_t *x, u_int64_t *y) {
+	struct ecc_point *pt  = ecc_alloc_point(4);
+	if (pt == NULL) return NULL;
+	pt->x[0] = x[0]; pt->x[1] = x[1]; pt->x[2] = x[2]; pt->x[3] = x[3];
+	pt->y[0] = y[0]; pt->y[1] = y[1]; pt->y[2] = y[2]; pt->y[3] = y[3];
+	return pt;
+}
+*/
+import "C"
+
+import (
+	//"math/big"
+	"unsafe"
+)
+
+// Functions implemented in ecc_asm_*64.s
+// Montgomery addition modulo P256
+func vliSub(left, right []uint64) (result []uint64, borrow bool) {
+	var res [4]uint64
+	ret := C.vli_sub((*C.u64)(&res[0]), (*C.u64)(&left[0]), (*C.u64)(&right[0]), 4)
+	result = res[:]
+	if ret != 0 {
+		borrow = true
+	}
+	return
+}
+
+// Montgomery inverse modulo P256
+func vliModInv(input, mod []byte) (result []uint64) {
+	var res [4]uint64
+	in := vliFromBE64(input)
+	modP := vliFromBE64(mod)
+	C.vli_mod_inv((*C.u64)(&res[0]), (*C.u64)(&in[0]), (*C.u64)(&modP[0]), 4)
+	result = res[:]
+	return
+}
+
+// Montgomery multiplication modulo P256
+func vliModMult(left, right, mod []byte) (result []uint64) {
+	var res [4]uint64
+	lf := vliFromBE64(left)
+	rt := vliFromBE64(right)
+	modP := vliFromBE64(mod)
+	C.vli_mod_mult_slow((*C.u64)(&res[0]), (*C.u64)(&lf[0]),
+		(*C.u64)(&rt[0]), (*C.u64)(&modP[0]), 4)
+	result = res[:]
+	return
+}
+
+func vliFromBE64(src []byte) (dest []uint64) {
+	var res [4]uint64
+	C.vli_from_be64((*C.u64)(&res[0]), unsafe.Pointer(&src[0]), 4)
+	dest = res[:]
+	return
+}
---
+++ ecc/ecc_test.go
@@ -0,0 +1,93 @@
+package ecc
+
+import (
+	"gitee.com/jkuang/go-fastecdsa/sm2"
+	"math/big"
+	"testing"
+)
+
+var (
+	x1, y1 *big.Int
+	x2, y2 *big.Int
+)
+
+func init() {
+	x1, _ = new(big.Int).SetString("12504736780261570232047283225790923734936736733904083456330261180763076903173", 16)
+	y1, _ = new(big.Int).SetString("81307578426096630754000956949482484966368962694792140728234975018259774469569", 16)
+	x2, _ = new(big.Int).SetString("48121564271922987841895377752074498583012355812029682461364979458450873405695", 16)
+	y2, _ = new(big.Int).SetString("37446874645719659508108418738243030372422533994743756508347851178597805285404", 16)
+}
+
+/*
+func TestRRbySM2(t *testing.T) {
+	n256 := new(big.Int).SetUint64(1)
+	n256.Lsh(n256, 256)
+	n := P256().Params().N
+	R := new(big.Int).Mod(n256, n)
+	RR := new(big.Int).Mul(R, R)
+	RR.Mod(RR, n)
+	ww := RR.Bits()
+	t.Logf("RR of sm2 is %x %x %x %x", ww[0], ww[1], ww[2], ww[3])
+	ww = n.Bits()
+	t.Logf("N(order) is %x %x %x %x", ww[0], ww[1], ww[2], ww[3])
+
+	p := P256().Params().P
+	Rinv := new(big.Int).SetUint64(1)
+	Rinv.Lsh(Rinv, 257)
+	Rinv.Mod(Rinv, p)
+	Rinv.ModInverse(Rinv, p)
+	t.Log("RInverse of sm2 is ", Rinv.Text(16))
+}
+*/
+
+func BenchmarkInverse(b *testing.B) {
+	b.ResetTimer()
+	p := sm2.P256().Params().P.Bytes()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_ = vliModInv(x1.Bytes(), p)
+		}
+	})
+}
+
+func BenchmarkMul(b *testing.B) {
+	b.ResetTimer()
+	p := sm2.P256().Params().P.Bytes()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = vliModMult(x1.Bytes(), x2.Bytes(), p)
+	}
+}
+
+/*
+func BenchmarkECADD(b *testing.B) {
+	b.ResetTimer()
+	curve := P256()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_, _ = curve.Add(x1, y1, x2, y2)
+		}
+	})
+}
+
+func BenchmarkECDBL(b *testing.B) {
+	b.ResetTimer()
+	curve := P256()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_, _ = curve.Double(x1, y1)
+		}
+	})
+}
+*/
