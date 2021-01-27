c6f6392aeec8ee227939ab8dc94efd1624deaa3f
Jesse Kuang <jkuang@21cn.com>
2021-01-27 17:06:01 +0800

    works on SM2 sign/verify

--- include/ecc_key.hpp
+++ include/ecc_key.hpp
@@ -44,26 +44,73 @@ public:
 	typedef bignum<4> felem_t;
 	private_key() = default;
 	template<typename curveT>
-	bool init(const curveT& curve, const felem_t& secret) {
+	private_key(const curveT& curve, const felem_t& secret,
+				const public_key& pk) : _pa(pk)
+	{
+		init(curve, secret);
+	}
+	template<typename curveT>
+	bool init(const curveT& curve, const felem_t& secret) noexcept {
 		curve.modN(_d, secret);
 		if (_d.is_zero()) return false;
 		_dInv = _d;
-		this->_dInv.uadd_to(1);
-		if (this->_dInv.is_zero()) return false;
+		_dInv.uadd_to(1);
+		curve.modN(_dInv, _dInv);
+		if (_dInv.is_zero()) return false;
 		// _dInv = (1 + d)^-1
 		mod_inv<4>(_dInv, _dInv, curve.paramN());
-		// calc public key
+		_inited = true;
 		return true;
+	}
+	template<typename curveT>
+	void calc_pa(const curveT& curve) noexcept {
+		if ( unlikely(!_inited) ) return;
+		point_t<4>	pt;
+		curve.scalar_mult_base(pt, _d);
+		_pa.x= pt.x;
+		_pa.y = pt.y;
 	}
 	const bignum<4>&	D() const noexcept { return *(&_d); }
 	const bignum<4>&	Di() const noexcept { return *(&_dInv); }
 	const public_key&	PA() const noexcept { return *(&_pa); }
 protected:
+	bool		_inited = false;
 	felem_t		_d;	// secret
 	felem_t		_dInv;	// (1+d)^-1  for SM2 sign
 	public_key	_pa;
 };
 
+
+/*
+ * SM2 sign/verify
+ * 			e = HASH (msg)
+ * 			Pa ...	Public key, Px/Py
+ * 			dA ...	private key
+ * 	sign
+ * 			k = random   256bits   Label getk
+ * 			x1, y1 = k * G
+ * 			r = e + x1
+ *			if r == 0 modN  goto getk
+ *			s = (1 + dA)^-1 * (k - r * dA)
+ *			if s == 0 modN goto getk
+ *			if r + s == 0 modN goto getk
+ *			return r, s, y1_is_odd
+ *
+ * 	verify
+ * 			t = r + s modN
+ * 			if t == 0 fail
+ * 			x2, y2 = s*G + t*Pa
+ * 			if x2 + e == r modN success else fail
+ *
+ *	recovery public key
+ *			Psx = r - e
+ *			Psy = get_Py ( Psx, Psy_is_odd )
+ *			Ps ...	Psx/Psy
+ *			u1 = (r + s)^-1
+ *			u2 = s * u1
+ *			Pa = (r+s)^1 * ( k*G - s*G) = (r+s)^-1 * Ps - s*(r+s)^-1 * G
+ *			Pa = u1 * Ps - u2 * G
+ */
 
 }	// namespace ecc
 
