be08a09e6aa6e8da27b3c3ffa48ccc0e2f0c4066
Jesse Kuang <jkuang@21cn.com>
2021-03-01 17:34:50 +0800

    btc arm64 asm EC add/double not work yet

--- sm2crypto/signature_nocgo.go
+++ sm2crypto/signature_nocgo.go
@@ -14,9 +14,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// +build nacl js !cgo
+// +build amd64 js !cgo
 
-package crypto
+package sm2crypto
 
 import (
 	"crypto/ecdsa"
